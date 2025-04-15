54d3f8e91867c2684161f1b703e9fb6f0a535577
kalifou <renetrao@gmail.com>
2025-04-15 15:00:22 +0200

    scripts: update benchmark

--- scripts/benchmark_esa.sh
+++ scripts/benchmark_esa.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+channel=15
 path_to_dataset=../data/ESA-ADB/data/preprocessed/multivariate/
-extension=/84_months.test.csv
+extension=/42_months.train.csv
+results_path=../results/benchmark_esa/
 
-for dataset in ESA-Mission1-semi-supervised ESA-Mission2-semi-supervised; do
-    for algorith_i in DAMP LOC IForest; do
+for dataset in ESA-Mission1-semi-supervised; do # ESA-Mission2-semi-supervised; do
+    for algorith_i in DAMP LOF IForest MatrixProfile; do
         echo $path_to_dataset$dataset$extension
-        python -m TSB_UAD.esa_adb -m $algorith_i -pthd $path_to_dataset$dataset$extension -ch 15 --n-jobs 1 -tm
+        python -m TSB_UAD.esa_adb -m $algorith_i -pthd $path_to_dataset$dataset$extension -ch $channel --n-jobs 1 -tm -pths $results_path
     done
 done
