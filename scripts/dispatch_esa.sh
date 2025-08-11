af3ee616f19ec25c4a931b5f8af71f43853b4f75
kalifou <renetrao@gmail.com>
2025-08-11 12:04:50 +0200

    fix benchmarking script: sequence of channels of interest.

--- scripts/dispatch_esa.sh
+++ scripts/dispatch_esa.sh
@@ -5,7 +5,7 @@
 
 
 
-for channel in $(seq 33 32 40 46 44 45 61 62 63); do
+for channel in 33 32 40 46 44 45 61 62 63; do
     for dataset in ESA-Mission1-semi-supervised; do # ESA-Mission2-semi-supervised; do
 	# Cheaper algorithm evaluations - 7 hours
 	for algorith_i_1 in DAMP IForest MatrixProfile SAND_offline PCA POLY CNN; do
